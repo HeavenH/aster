cc42f3c3a17ae76b8f250ceadc34bed139cbeee8
gilmar silva <gilmarheavensilva@hotmail.com>
2022-12-18 00:09:00 -0300

    chore: enable cors again

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   async headers() {
     return [
       {
@@ -7,11 +8,13 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://aster-7bqvewloi-heavenh.vercel.app/" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
       }
     ]
   }
-};
+}
+
+module.exports = nextConfig
