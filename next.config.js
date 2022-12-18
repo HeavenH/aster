c3dc0868f92f9a7a50659257269fa96e48cad7bd
gilmar silva <gilmarheavensilva@hotmail.com>
2022-12-18 00:00:15 -0300

    chore: enable cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://aster-woad.vercel.app/:path*',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
+
